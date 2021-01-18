25522488fb368d588595a2ab727b58be98357d99
Andrei <binofaata@gmail.com>
2021-01-18 22:49:23 +0400

    Add some new tasks

--- src/main/java/CollectionTasks.java
+++ src/main/java/CollectionTasks.java
@@ -1,17 +1,63 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
 
 
 public class CollectionTasks {
     public static void main(String[] args) {
 //        outLessFive();
-        unionCollection();
+//        retainCollection();
+//        sortMinMax();
+//        unionCollection();
+        threeKeyMax();
     }
+    static void threeKeyMax(){
+        Map<String, Integer> myDictionary = new HashMap<>();
+        myDictionary.put("a", 500);
+        myDictionary.put("b", 5874);
+        myDictionary.put("c",560);
+        myDictionary.put("d",400);
+        myDictionary.put("e",5874);
+        myDictionary.put("f",20);
+
+//        System.out.println(myDictionary);
+        Collection<Integer> values = myDictionary.values();
+        ArrayList<Integer> listValues = new ArrayList<>();
+        ArrayList<Integer> threeMaxValues = new ArrayList<>();
+
+        listValues.addAll(values);
+//        System.out.println(values);
+        System.out.println(Collections.max(listValues));
+        }
+
     static void unionCollection(){
+        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1,1,2,3,5,8,13,21,34,55,89));
+        ArrayList<Integer> b = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
+        ArrayList<Integer> c = new ArrayList<>();
+        c.addAll(a);
+        c.addAll(b);
+        System.out.println(c);
+    }
+    static void sortMinMax(){
+        ArrayList<Integer> sortMinMax = new ArrayList<>(Arrays.asList(1,24,2,3,53,8,13,21,34,55,89));
+        System.out.println("До сортировки: ");
+        for(Integer counter: sortMinMax){
+            System.out.print(counter+" ");
+        }
+        System.out.println();
+        System.out.println("После сортировки от меньшего к большему: ");
+        Collections.sort(sortMinMax);
+        for(Integer counter: sortMinMax){
+            System.out.print(counter+" ");
+        }
+        System.out.println();
+        System.out.println("После сортировки от большего к меньшему: ");
+        Collections.sort(sortMinMax, Collections.reverseOrder());
+        for(Integer counter: sortMinMax){
+            System.out.print(counter+" ");
+        }
+    }
+    static void retainCollection(){
         ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1,1,2,3,5,8,13,21,34,55,89));
         ArrayList<Integer> b = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
         a.retainAll(b);
